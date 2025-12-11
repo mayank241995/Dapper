a5941c282a7322b0ea50d81d9793588fadd30763
mayank241995 <srivastava.mayank241995@gmail.com>
2025-12-11 07:22:50 +0530

    Add project files.

---
+++ Dapper/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dapper.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Company> Companies { get; set; }
+    }
+}
