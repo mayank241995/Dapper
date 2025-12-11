29009b705de6fa717bc1b9a4b726b2b75569beb8
mayank241995 <srivastava.mayank241995@gmail.com>
2025-12-11 08:10:26 +0530

    setup scafffold

---
+++ Dapper/Repository/ICompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Models;
+
+namespace Dapper.Repository
+{
+    public interface ICompanyRepository
+    {
+        Company Find(int id);
+        List<Company> GetAll();
+        Company Add(Company company);
+        Company Update(Company company);
+        void Remove(int id);
+    }
+}
